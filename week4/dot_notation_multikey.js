13a3ccd3039913c25e11ee48edf27dddbf8250c3
Em01 <emma_williams1@hotmail.co.uk>
2015-07-10 18:00:49 +0100

    Further work on Dot notation and Multikey

--- week4/dot_notation_multikey.js
+++ week4/dot_notation_multikey.js
@@ -14,3 +14,29 @@ db.students.explain().find({'scores': {$elemMatch: {type:'exam', score:{'$gt':99
 //do not use the following query-may not satisfy criteria in the same sub part
 
 db.students.find({'$and':[{'scores.type':'exam'},{'scores.score':{'$gt':99.8}}]}).pretty();
+
+
+// Suppose you have a collection called people in the database earth with documents of the following form:
+
+{
+	"_id" : ObjectId("551458821b87e1799edbebc4"),
+	"name" : "Eliot Horowitz",
+	"work_history" : [
+		{
+			"company" : "DoubleClick",
+			"position" : "Software Engineer"
+		},
+		{
+			"company" : "ShopWiki",
+			"position" : "Founder & CTO"
+		},
+		{
+			"company" : "MongoDB",
+			"position" : "Founder & CTO"
+		}
+	]
+}
+
+
+db.people.createIndex({"work_history.company":-1});
+
